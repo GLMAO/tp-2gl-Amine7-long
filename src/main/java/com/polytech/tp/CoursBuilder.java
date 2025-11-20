6477c4b3320edd946f3eb3f2ae021594ce41f86a
Amine Dadi <amineroubehie.com>
2025-11-20 17:16:19 +0100

    TP GL2 : version final

--- src/main/java/com/polytech/tp/CoursBuilder.java
+++ src/main/java/com/polytech/tp/CoursBuilder.java
@@ -1,14 +1,67 @@
 package com.polytech.tp;
 
 public class CoursBuilder {
-    // TODO: Implémenter le pattern Builder
-    
-    
-    public CoursBuilder setMatiere(String matiere) { return this; }
-    public CoursBuilder setEnseignant(String enseignant) { return this; }
-    
-    
+    private String matiere;
+    private String enseignant;
+    private String salle;
+    private String date;
+    private String heureDebut;
+    private boolean estOptionnel;
+    private String niveau;
+    private boolean necessiteProjecteur;
+
+    // Valeurs par défaut
+    public CoursBuilder() {
+        this.salle = "A définir";
+        this.date = "A définir";
+        this.heureDebut = "08h00";
+        this.estOptionnel = false;
+        this.niveau = "Licence";
+        this.necessiteProjecteur = false;
+    }
+
+    public CoursBuilder setMatiere(String matiere) {
+        this.matiere = matiere;
+        return this;
+    }
+
+    public CoursBuilder setEnseignant(String enseignant) {
+        this.enseignant = enseignant;
+        return this;
+    }
+
+    public CoursBuilder setSalle(String salle) {
+        this.salle = salle;
+        return this;
+    }
+
+    public CoursBuilder setDate(String date) {
+        this.date = date;
+        return this;
+    }
+
+    public CoursBuilder setHeureDebut(String heureDebut) {
+        this.heureDebut = heureDebut;
+        return this;
+    }
+
+    public CoursBuilder setEstOptionnel(boolean estOptionnel) {
+        this.estOptionnel = estOptionnel;
+        return this;
+    }
+
+    public CoursBuilder setNiveau(String niveau) {
+        this.niveau = niveau;
+        return this;
+    }
+
+    public CoursBuilder setNecessiteProjecteur(boolean necessiteProjecteur) {
+        this.necessiteProjecteur = necessiteProjecteur;
+        return this;
+    }
+
     public Cours build() {
-        return null;
+        return new Cours(matiere, enseignant, salle, date, heureDebut,
+                estOptionnel, niveau, necessiteProjecteur);
     }
 }
